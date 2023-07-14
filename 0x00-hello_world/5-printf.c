6d8390099fadb962e54bcff55ff136bb85bf1c3b
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-14 13:32:35 -0700

    hot fix for c files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
-    printf("with proper grammer, but the outcome is a piece os art,\n");
-    return 0;
+/**
+ * main - Entry point
+ * Description: return given string using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece os art,\n");
+return (0);
 }
