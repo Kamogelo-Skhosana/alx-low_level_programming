80f4910019a11135f7c9979d9e719536ad7fc849
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-14 20:56:59 -0700

    last changes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * printing lowercase letters
+ */
+int main(void)
+{
+char letters
+for (letters = "a"; letters <= "z"; letters++)
+{
+putchar(letters);
+}
+putchar("\n");
+return (0)
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Alx c variacles, if, else, while
