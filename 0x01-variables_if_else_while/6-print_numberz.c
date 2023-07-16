5cec0ebfdababcd93639236d01c4d032160e3980
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-16 07:26:21 -0700

    2nd commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,7 @@
  */
 int main(void)
 {
-putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
-putchar('\n');
+putchar('0123456789');
+utchar('\n');
 return (0);
 }
