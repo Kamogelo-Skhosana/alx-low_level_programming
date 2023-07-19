0beb0b2d8b280dfcca34ecd570d9a930d848221b
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-19 09:00:33 -0700

    last correction

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,25 @@
-#include <stdio.h>
 /* 0. Print _putchar followed by a new line */
 int _putchar(char c);
 /* 1. Print the alphabet in lowercase */
 void print_alphabet(void);
+/* 2. Print 10 times the alphabet in lowercase */
+void print_alphabet_x10(void);
+/* 3. Check if a character is lowercase */
+int _islower(int c);
+/* 4. Check if a character is alphabetic */
+int _isalpha(int c);
+/* 5. Print the sign of a number */
+int print_sign(int n);
+/* 6. Compute the absolute value of an integer */
+int _abs(int n);
+/* 7. Print the last digit of a number */
+int print_last_digit(int n);
+/* 8. Print every minute of the day */
+void jack_bauer(void);
+/* 9. Print the 9 times table */
+void times_table(void);
+/* 10. Add two integers and return the result */
+int add(int a, int b);
+/* 11. Print natural numbers from n to 98 */
+void print_to_98(int n);
+
