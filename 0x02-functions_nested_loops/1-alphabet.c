fd8d1490cd2536038a00bd2cea37117217a47a35
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-19 09:19:55 -0700

    5th commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
 #include "main.h"
-#include "_putchar.c"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
 void print_alphabet(void)
 {
 char c;
@@ -16,6 +12,5 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-}
 return (0);
 }
