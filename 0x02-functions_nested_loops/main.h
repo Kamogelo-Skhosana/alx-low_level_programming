a59a0fcebc1b2577687bf8ff25ea2b3e8abf62a7
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-18 20:09:18 -0700

    2nd commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,22 @@
  * Return: Always 0 (Success)
  */
 int _putchar(char c);
+void print_alphabet(void)
+{
+chat letter ='a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+for (i = 0; i < 10; i++)
+{
+letter = 'a';
+print_alphabet();
+}
