710b4a07bb6e7b00ebdcdda586bf00893769f7be
Kamogelo <skhosanakamogelo634@gmail.com>
2023-07-19 10:32:39 -0700

    4th commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - print sign of a number
  * @n: the variable
- * retur : 1 n, 0 or -1 n
+ * return : 1 n, 0 or -1 n
  */
 int print_sign(int n)
 {
@@ -16,7 +16,7 @@ else if (n == 0)
 _putchar('0');
 return (0);
 }
-else 
+else
 {
 _putchar('-');
 return (-1);
