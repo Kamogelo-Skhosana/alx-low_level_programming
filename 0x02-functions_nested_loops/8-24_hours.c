0c4c6ba1a8a8b7da707781e4b47b4423a17ffe7e
Kamogelo Skhosana <skhosanakamogelo634@gmail.com>
2023-08-03 07:29:45 -0700

    return correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-putchar((hour / 10) + '0');
+_putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minute / 10) + '0');
