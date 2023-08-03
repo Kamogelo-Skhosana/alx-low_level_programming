e4f2e6c8560e434cc8f32e2708bb5c65d1deed38
Kamogelo Skhosana <skhosanakamogelo634@gmail.com>
2023-08-03 07:39:11 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,14 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 result = i * j;
-if (j != 0)
+if (j == 0)
+{
+_putchar('0');
+}
+else if (result <= 9)
 {
 _putchar(',');
 _putchar(' ');
-}
-if (result <= 9)
-{
 _putchar(' ');
 }
 else
