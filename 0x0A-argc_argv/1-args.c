f7451f8930d20d8dc8a6543637fa72cf8f776ed0
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 07:50:59 -0700

    Task 1 correction 3

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry Point
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 printf("%d\n", argc - 1);
-return 0;
+return (0);
 }
