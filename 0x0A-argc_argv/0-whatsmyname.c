7b58c6df82da3820aafd7f7c487b9a9a7fa45bc7
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 07:25:29 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+{
+printf("%s\n", argv[0]);
+}
+return 0;
+}
+
