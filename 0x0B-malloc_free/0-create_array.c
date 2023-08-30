b440104db7ae42df2922f060b5133d99f3fee714
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 11:52:45 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,33 +11,16 @@ char *create_array(unsigned int size, char c)
 {
 if (size == 0)
 {
-return NULL;
+return (NULL);
 }
-char *array = (char *)malloc(size * sizeof(char));
+char *array = (char *)malloc(size);
 if (array == NULL)
 {
-return NULL;
+return (NULL); // Memory allocation failed
 }
 for (unsigned int i = 0; i < size; i++)
 {
 array[i] = c;
 }
 return (array);
-}
-int main()
-{
-unsigned int size = 10;
-char character = 'A';
-char *result = create_array(size, character);
-if (result == NULL)
-{
-return (1);
-}
-for (unsigned int i = 0; i < size; i++)
-{
-printf("%c ", result[i]);
-}
-printf("\n");
-free(result);
-return (0);
 }
