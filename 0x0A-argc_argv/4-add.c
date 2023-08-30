b3454dbd4e1e85e43b4992e1a70d9f4d55572098
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 08:43:42 -0700

    Task 4 correction 3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc < 2)
-{
-return (printf("0\n"), 0);
-}
-int i, j, total = 0;
+int i, j, sum = 0;
 for (i = 1; i < argc; i++)
 {
-for (j = 0; arg[i] != '\0'; j++)
+for (j = 0; argv[i][j]; j++)
 {
-if (arg[j] < '0' || arg[j] > '9')
-{
+if (argv[i][j] < '0' || argv[i][j] > '9')
 return (printf("Error\n"), 1);
 }
+sum += atoi(argv[i]);
 }
-total += atoi(arg);
-}
-return (printf("%d\n", total), 0);
+printf("%d\n", sum);
+return (0);
 }
