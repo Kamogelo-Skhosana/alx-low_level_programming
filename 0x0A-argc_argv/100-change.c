a031f743eab9af8f32ea3a1ee45f8e749498816b
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 09:33:19 -0700

    Advanced Task 100 correction 6

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,12 +13,14 @@ int denominations[5];
 int num_denominations;
 if (argc != 2)
 {
-return (printf("Error\n"), 1);
+printf("Error\n");
+return 1;
 }
 cents = atoi(argv[1]);
 if (cents < 0)
 {
-return (printf("0\n"), 0);
+printf("0\n");
+return 0;
 }
 denominations[0] = 25;
 denominations[1] = 10;
@@ -31,5 +33,6 @@ for (i = 0; i < num_denominations && cents; i++)
 coins += cents / denominations[i];
 cents %= denominations[i];
 }
-return(printf("%d\n", coins), 0);
+printf("%d\n", coins);
+return (0);
 }
