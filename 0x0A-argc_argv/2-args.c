30079e7a54067629535c03e38660086f31ecc0c2
Kamogelo <skhosanakamogelo634@gmail.com>
2023-08-30 07:57:01 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+for (int i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
